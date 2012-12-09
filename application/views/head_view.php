36f80e5346db565b353a1f960c69e6b50648a165
brneese <brneese@brneese.com>
2012-12-08 18:15:20 -0600

    banish google analytics

--- application/views/head_view.php
+++ application/views/head_view.php
@@ -30,20 +30,7 @@
     	<script type="text/javascript" src="/js/ui/jquery.ui.map.services.js"></script>
     	<script type="text/javascript" src="/js/ui/jquery.ui.map.extensions.js"></script>
 
-    	<!-- google analytics -->
-
-    	<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-29118751-1']);
-		  _gaq.push(['_trackPageview']);
-
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
+ 
 
 	<!-- tities! I mean, uh, titles. -->
 	<title>Kikr [alpha]</title>
